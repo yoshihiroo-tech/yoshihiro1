2eeee6e09849fc4b0967248d2da9a6e8957f78f7
yoshihiro.uda <yoshihiro7319@yahoo.co.jp>
2020-01-30 23:53:04 +0900

    Initial Commit

---
+++ yoshihiro1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  yoshihiro1
+//
+//  Created by 宇田 好宏 on 2020/01/30.
+//  Copyright © 2020 Yoshihiro Uda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
